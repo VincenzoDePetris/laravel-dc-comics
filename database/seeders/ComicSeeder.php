97a957bf0d58a52bdb5f53a32991744cc21cc7f1
VincenzoDePetris <vince02@live.it>
2023-10-19 16:17:35 +0200

    add: data in database

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Comic;
+
 class ComicSeeder extends Seeder
 {
     /**
@@ -14,6 +16,18 @@ class ComicSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $_comics = config("comics");
+
+        foreach ($_comics as $_comic) {
+            $comic = new Comic();
+            $comic->title = $_comic['title'];
+            $comic->description = $_comic['description'];
+            $comic->thumb = $_comic['thumb'];
+            $comic->price = $_comic['price'];
+            $comic->series = $_comic['series'];
+            $comic->sale_date = $_comic['sale_date'];
+            $comic->type = $_comic['type'];
+            $comic->save();
+        }
     }
 }
